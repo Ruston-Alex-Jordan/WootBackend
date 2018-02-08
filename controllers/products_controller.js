df6a7e41e15c7660a1bd89884778348954fe8d00
Ruston Reformado <rustonreformado@gmail.com>
2018-02-08 14:26:07 -0700

    fixed issue with db in controller products

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -1,5 +1,6 @@
 module.exports = {
     getProducts: (req, res, next) => {
+        const db = req.app.get('db');
         db.get_products()
             .then( products => { res.status(200).send(products); })
             .catch( err => {
