a54ee7af4596897680e8538a6f01532c6566c89d
Ruston Reformado <rustonreformado@gmail.com>
2018-02-09 23:27:35 -0700

    fixed issue with massive connection string

--- server.js
+++ server.js
@@ -11,18 +11,11 @@ const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
 
-massive({
-    host: process.env.HOST,
-    port: process.env.PORT,
-    database: process.env.DATABASE,
-    user: process.env.USER,
-    password: process.env.PASSWORD,  
-    ssl: true
-}).then(function (db) {
+massive( process.env.CONNECTION_STRING ).then( (db) => {
     app.set('db', db);
-    
-    app.get('/products', products_controller.getProducts);
+
 })
+app.get('/products', products_controller.getProducts);
 
 const port = 8000;
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
