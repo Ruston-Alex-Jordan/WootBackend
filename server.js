b56b6f51524da005156bd5e23ea2162565db9c00
Ruston Reformado <rustonreformado@gmail.com>
2018-02-21 18:33:45 -0700

    In progress, pair programming with frontend dev

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const massive = require('massive');
 
 const products_controller = require('./controllers/products_controller.js');
+const cart_controller = require('./controllers/cart_controller.js');
 
 const app = express();
 app.use( bodyParser.json() );
@@ -20,6 +21,9 @@ app.get('/products', products_controller.getProducts);
 
 //put api calls
 app.put('/update-quantity', products_controller.updateProducts);
+
+//post api calls
+app.post('/order-complete', cart_controller.orderComplete);
 
 
 const port = 8000;
