4af1bc64e617103ef535168ac54c5d2b8b6efabc
Ruston Reformado <rustonreformado@gmail.com>
2018-02-21 19:51:03 -0700

    orders table created, inserting new orders via backend logic

--- controllers/cart_controller.js
+++ controllers/cart_controller.js
@@ -1,10 +1,29 @@
 module.exports = {
     orderComplete: (req, res, next) => {
         const db = req.app.get('db');
-        db.order_complete().then()
-        .then( () => { res.status(200).send('cats hobbes'); })
-        .catch( err => {
-            res.status(500).send(err);
+        let config = req.body;
+        console.log(config);
+
+        let productID = config.cart.map( (e) => {
+            return e.productid
+        });
+        let total = function () {
+            let subtotal = 0;
+            for(let i = 0; i < config.cart.length; i++){
+                subtotal += config.cart[i].saleprice
+            }
+            return subtotal;
+        };
+        let email = config.token.card.email;
+        let cardLastFour = config.token.card.last4;
+        let addressCity = config.token.card.address_city;
+        let addressLine1 = config.token.card.address_line1;
+        let addressState = config.token.card.address_state;
+        let addressZip = config.token.card.address_zip;
+
+        db.order_complete([ productID, total, email, cardLastFour, addressCity, addressLine1, addressState, addressZip ])
+            .then( () => { res.status(200).send('success'); })
+            .catch( err => { res.status(500).send(err);
         });
     }
 }
--- db/order_complete.sql
+++ db/order_complete.sql
@@ -1,1 +1,2 @@
-select * from orders;
+INSERT INTO orders ( productID, total, email, cardLastFour, addressCity, addressLine1, addressState, addressZip) 
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
