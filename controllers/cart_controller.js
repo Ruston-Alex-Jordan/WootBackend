f70a7637c8b458ed9ea738a4466f7fa66b70ac8c
Ruston Reformado <rustonreformado@gmail.com>
2018-02-21 18:37:39 -0700

    in progress

--- controllers/cart_controller.js
+++ controllers/cart_controller.js
@@ -1,7 +1,10 @@
 module.exports = {
     orderComplete: (req, res, next) => {
         const db = req.app.get('db');
-        db.order_complete()
-        console.log('asdlkfjalskdfjlasjdflkasjdflakjsdflk')
+        db.order_complete().then()
+        .then( () => { res.status(200).send('cats hobbes'); })
+        .catch( err => {
+            res.status(500).send(err);
+        });
     }
 }
